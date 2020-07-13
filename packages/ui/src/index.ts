c389105050214e4e2695b317b385e1fc609b4eef
刘放 <362512489@qq.com>
2020-07-13 20:14:34 +0800

    feat(ui): questions based on inquirer

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,2 +1,3 @@
-export { tableContent } from './table';
+export { tableContent } from "./table";
 export { logger, createLogger } from "./logger";
+export { input, numberInput, passwordInput, checkbox, radio, confirm } from "./question";
