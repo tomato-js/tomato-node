07c96fe20a8b24191abb97b78288e75470a8e0c8
刘放 <362512489@qq.com>
2020-09-24 19:33:02 +0800

    feat(fs): exists

--- packages/fs/src/index.ts
+++ packages/fs/src/index.ts
@@ -2,3 +2,4 @@ export { readJson, readJsonSync } from "./read-json";
 export { writeJson, writeJsonSync } from "./write-json";
 export { requireModule, isModuleExist } from "./require-module";
 export { isDir, isFile } from "./is";
+export { exists } from "./exists";
